722e1467d10c68668c7993c5f1a2bee6a61d0865
justNo4b <69092138+justNo4b@users.noreply.github.com>
2023-08-25 21:57:50 +0300

    Various improvements
    1. Allow swithing between batched / segmented modes
    2. Allow DPI via CL. Default DPI = 300 now
    3. ReverseComplement mode via CL
    4. Add arrows
    5. Distinguish between + and - ORF in arrows
    6. Better RdRp detection

--- draw.py
+++ draw.py
@@ -8,23 +8,86 @@ from Bio import SeqIO
 
 padding_top         = 25
 padding_left        = 45
-padding_ORF         = 25
-padding_ORF_level   = 30
+padding_ORF         = 30
+padding_ORF_level   = 50
 padding_text        = 15
 padding_text_x      = 10
 padding_cap_y       = 37
+ORF_width           = 13
+line_width          = 5
+isOnlyOne           = False
+scale               = 1
 
-ORF_width     = 13
-line_width    = 5
-isOnlyOne     = False
+
+def _genSeqName(seq):
+    s = ""
+    s += seq.annotations["organism"]
+    return s
+
+def _drawSegText(seqdata, batchmode, num):
+    if (not batchmode):
+        if (not isOnlyOne): drawObject.text((padding_left - padding_text, y - padding_cap_y), text=("Segment " + str(num)), fill="Black", font=header_font, anchor="la")
+    # in batch mode treat each sequence as a separate virus
+    # in that case pull name out of seq data
+    else:
+        drawObject.text((padding_left - padding_text, y - padding_cap_y), text=seqdata.name, fill="Black", font=header_font, anchor="la")
+    return
+
+def _defineStrandDirection(rcRNA):
+    global leftGenomeText
+    global rightGenomeText
+    if (rcRNA == True):
+        leftGenomeText  = "3`"
+        rightGenomeText = "5`"
+    else:
+        leftGenomeText  = "5`"
+        rightGenomeText = "3`"
+
+
+def _drawComplementORF(drawObj, orf, currY, txtFont):
+    y_ORF = currY + padding_ORF + padding_ORF_level * orf.drawlevel
+    x1_ORF = padding_left + orf.start * scale
+    x2_ORF = x1_ORF + orf.length * scale
+    x_caption = int((x2_ORF + x1_ORF) / 2)
+    y_caption = y_ORF
+    x1_ORF_line = x1_ORF + ORF_width * 3
+    x1_ORF_rect = x1_ORF + ORF_width * 2
+    # отрисовка наших геномных блоков
+    drawObj.rectangle([(x1_ORF_rect, y_ORF - ORF_width), (x2_ORF, y_ORF + ORF_width)], orf.color, outline="Black")
+    drawObj.regular_polygon(bounding_circle=(x1_ORF_rect, y_ORF, ORF_width * 2), n_sides=3, rotation=90, fill=orf.color, outline="Black" )
+    drawObj.line([(x1_ORF_line, y_ORF - ORF_width), (x1_ORF_line, y_ORF + ORF_width)], orf.color, width=1)
+    # отрисовка подписей
+    drawObj.text((x_caption, y_caption),  orf.name, fill="White", font=txtFont, anchor="mm")
+
+    return
+
+def _drawNormalORF(drawObj, orf, currY, txtFont):
+    y_ORF = currY + padding_ORF + padding_ORF_level * orf.drawlevel
+    x1_ORF = padding_left + orf.start * scale
+    x2_ORF = x1_ORF + orf.length * scale
+    x_caption = int((x2_ORF + x1_ORF) / 2)
+    y_caption = y_ORF
+    x2_ORF_line = x2_ORF - ORF_width * 3
+    x2_ORF_rect = x2_ORF - ORF_width * 2
+    # отрисовка наших геномных блоков
+    drawObject.rectangle([(x1_ORF, y_ORF - ORF_width), (x2_ORF_rect, y_ORF + ORF_width)], orf.color, outline="Black")
+    drawObj.regular_polygon(bounding_circle=(x2_ORF_rect, y_ORF, ORF_width * 2), n_sides=3, rotation=270, fill=orf.color, outline="Black" )
+    drawObj.line([(x2_ORF_line, y_ORF - ORF_width), (x2_ORF_line, y_ORF + ORF_width)], orf.color, width=1)
+
+
+    # отрисовка подписей
+    drawObj.text((x_caption, y_caption),  orf.name, fill="White", font=txtFont, anchor="mm")
+
+    return
 
 #define containers
 workload     = []
-segmentSizes = []
+usedSequences = []
 ORF_array    = []
 
-##define ORF structure
-ORF_struct = namedtuple("ORF_struct", "name start length drawlevel color")
+##define structures
+ORF_struct = namedtuple("ORF_struct", "name start length isCompl drawlevel color")
+SEQ_data   = namedtuple("SEQ_data", "name length")
 
 #parse some arguments
 parser = argparse.ArgumentParser()
@@ -32,37 +95,51 @@ parser.add_argument("-seg", "--file_input", nargs="*", type=str)
 parser.add_argument("-w", "--width", type=int, default=700)
 parser.add_argument("-n", "--name", type=str, default="Virus name")
 parser.add_argument("-o", "--output", type=str, default="default")
-workload = parser.parse_args().file_input
-xSize = parser.parse_args().width
-xName = parser.parse_args().name
-xOutput = parser.parse_args().output
+parser.add_argument("-rc", "--reverse_compl", action="store_true", help="Drawn image is reverse-complement")
+parser.add_argument("-dpi", "--image_dpi", type=int, default=300, help="DPI of the obtained image, 300 is default")
+parser.add_argument("-b", "--batch_mode", action="store_true", help="Treat each gb sequence as a separate virus")
+
+workload    = parser.parse_args().file_input
+xSize       = parser.parse_args().width
+xName       = parser.parse_args().name
+xOutput     = parser.parse_args().output
+rcRNA       = parser.parse_args().reverse_compl
+dpiUsed     = parser.parse_args().image_dpi
+isBatch     = parser.parse_args().batch_mode
+
+
+leftGenomeText  = ""
+rightGenomeText = ""
+
 #if outputfile dont specified and name is specified, set output name the same as name
 if ((xOutput == "default") and (xName != "Virus name")):
     xOutput = xName
 
-#calculate some important stuff
-seg_count   = len(workload)
-thinkness   = padding_ORF / 2 + ORF_width + padding_ORF + padding_cap_y
-#y-size с запасом, чтобы всё точно влезло
-ySize       = int (padding_top + thinkness * (seg_count + 1) * 1.5)
+
+_defineStrandDirection(rcRNA)
 
 
 #####
 # Work with seq
-fileCount = 0
+seqCount = 0
 for file in workload:
-    fileCount = fileCount + 1
     for sequence in SeqIO.parse(file, "gb"):
-        segmentSizes.append(len(sequence.seq))
+        seqCount = seqCount + 1
+        sLen = len(sequence.seq)
+        sName = _genSeqName(sequence)
+        newData = SEQ_data(sName, sLen)
+        usedSequences.append(newData)
         allORFs  = []
         lastend = 0
         for j in range(0, len(sequence.features)):
             if(sequence.features[j].type == "CDS"):
-                orfLevel = 0
-                orfName  = ""
-                orfColor = "Purple"
-                orfStart = int(sequence.features[j].location.start)
-                orfEnd   = int(sequence.features[j].location.end)
+                orfLevel    = 0
+                orfName     = ""
+                orfColor    = "Purple"
+                orfStart    = int(sequence.features[j].location.start)
+                orfEnd      = int(sequence.features[j].location.end)
+                # Note -1 is complement, 1 is normal
+                orfIsCompl = sequence.features[j].location.strand
                 if (lastend >= orfStart):
                     orfLevel = 1
                 lastend  = orfEnd
@@ -81,21 +158,26 @@ for file in workload:
                     orfName = orfProd
                 else:
                     orfName = "Unknown"
-                if (orfProd == "RdRp" or orfProd == "polymerase"):
+                if (("RdRp" in orfProd) or ("polymerase" in orfProd)):
                     orfColor = "Green"
-                newOrf = ORF_struct(orfName, orfStart , orfEnd - orfStart, orfLevel, orfColor)
+                newOrf = ORF_struct(orfName, orfStart , orfEnd - orfStart, orfIsCompl, orfLevel, orfColor)
                 allORFs.append(newOrf)
         ORF_array.append(allORFs)
 
-if (fileCount == 1):
+if (seqCount == 1):
     isOnlyOne = True
 
+
+#calculate some important stuff
+thinkness   = padding_ORF / 2 + ORF_width + padding_ORF + padding_cap_y
+#y-size с запасом, чтобы всё точно влезло
+ySize       = int (padding_top + thinkness * (seqCount + 1) * 1.5)
 # найти масштаб, используя максимальную длину сгмента и xSize
 # сперва найти самый длинный сегмент
 max_length = 0
-for i in range(0, len(segmentSizes)):
-    if (segmentSizes[i] > max_length):
-        max_length = segmentSizes[i]
+for i in range(0, len(usedSequences)):
+    if (usedSequences[i].length > max_length):
+        max_length = usedSequences[i].length
 
 # далее отмасштабировать, предполагая что мы хотим оставить по
 # крайней мере padding справа и слева
@@ -118,32 +200,24 @@ maxLevel = 0
 
 # Name on the top of it
 drawObject.text((padding_left, padding_top), text=xName, fill="Black", font=title_font, anchor="la")
-for i in range(0, len(segmentSizes)):
+for i in range(0, len(usedSequences)):
     # calculate positions
     c = i + 1
     y = nextSegY + thinkness
-    x2 = padding_left + segmentSizes[i] * scale
+    x2 = padding_left + usedSequences[i].length * scale
     #отрисовка общих вещей - геном, размер etc
     drawObject.line([(padding_left, y), (x2, y)], fill="Black", width=line_width)
-    drawObject.text((padding_left - padding_text, y - padding_text), text="5`", fill="Black", font=header_font, anchor="la")
-    drawObject.text((int(x2 + padding_text / 2), y - padding_text), text="3`", fill="Black", font=header_font, anchor="la")
-    if (not isOnlyOne):
-        drawObject.text((padding_left - padding_text, y - padding_cap_y), text=("Segment " + str(c)), fill="Black", font=header_font, anchor="la")
-    drawObject.text((x2, y + 5),  text = str(segmentSizes[i]), fill="Black", font=number_font, anchor="la")
+    drawObject.text((padding_left - padding_text, y - padding_text), text=leftGenomeText, fill="Black", font=header_font, anchor="la")
+    drawObject.text((int(x2 + padding_text / 2), y - padding_text), text=rightGenomeText, fill="Black", font=header_font, anchor="la")
+    drawObject.text((x2, y + 5),  text = str(usedSequences[i].length), fill="Black", font=number_font, anchor="la")
+    _drawSegText(usedSequences[i], isBatch, c)
     #отрисовка ORF
     for orf in ORF_array[i]:
-        y_ORF = y + padding_ORF + padding_ORF_level * orf.drawlevel
-        x1_ORF = padding_left + orf.start * scale
-        x2_ORF = x1_ORF + orf.length * scale
-        x_caption = int((x2_ORF + x1_ORF) / 2) - padding_text * 2
-        y_caption = int(y_ORF - ORF_width / 2)
-        # отрисовка наших геномных блоков
-        drawObject.rectangle([(x1_ORF, y_ORF - ORF_width), (x2_ORF, y_ORF + ORF_width)], orf.color, outline="Black")
-        # отрисовка подписей
-        drawObject.text((x_caption, y_caption),  orf.name, fill="White", font=number_font, anchor="la")
+        if (orf.isCompl == 1): _drawNormalORF(drawObject, orf, y, number_font)
+        else: _drawComplementORF(drawObject, orf, y, number_font)
         nextSegY = nextSegY + padding_ORF_level * orf.drawlevel
     nextSegY = nextSegY + thinkness
 
 # в конце не забыть удалить drawObject
 del drawObject
-image.save(xOutput, "PNG")
+image.save(xOutput, "PNG", dpi=(dpiUsed,dpiUsed))
