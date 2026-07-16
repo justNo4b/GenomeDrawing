56724820bf77dd50560ab2cff6f8066430893add
justNo4b <69092138+justNo4b@users.noreply.github.com>
2026-07-16 15:23:09 +0300

    Current dev rewrite (#4)
    * Add files via upload
    
    * Add current dev
    
    Current dev version

--- draw.py
+++ draw.py
@@ -8,8 +8,8 @@ from Bio import SeqIO
 #define draw parameters
 
 padding_top         = 25
-padding_left        = 45
-padding_ORF         = 30
+padding_left        = 42
+padding_ORF         = 40
 padding_ORF_level   = 50
 padding_text        = 15
 padding_text_x      = 10
@@ -22,14 +22,19 @@ scale_horizontal    = 1
 leftGenomeText      = ""
 rightGenomeText     = ""
 num_font_size       = 13
-head_font_size      = 16
-title_font_size     = 18
+head_font_size      = 19
+title_font_size     = 20
+font_file           = "OpenSans-Regular.ttf"
 font_circular_scale = 2
 
 
-def _genSeqName(seq):
+def _genSeqName(seq, filetype):
     s = ""
-    s += seq.annotations["organism"]
+    if (filetype == "gb"):
+        s += seq.annotations["organism"]
+    elif(filetype == "snapgene"):
+        s += seq.description    
+    ##
     return s
 
 
@@ -174,7 +179,8 @@ def _drawNormal(i,usedSequences):
 ###################################################
 # Helpers for circular drawings
 
-def _getArrowStart(circle, rotation, rotation_adj):
+def _getArrowStart(circle, rotation, rotation_adj, isRC):
+    if (isRC): rotation_adj = -rotation_adj
     center_x = circle[0] + (circle[2] - circle[0]) / 2
     center_y = circle[1] + (circle[3] - circle[1]) / 2
     radius = (circle[2] - circle[0] - ORF_width * 2) / 2
@@ -256,9 +262,18 @@ def _drawCircular(i,usedSequences):
 
         # отрисовка наших геномных блоков
         adj = _getArrowArcAdj(circle_xy_a[circ_level])
-        new  = _getArrowStart(circle_xy_a[circ_level], d_end, adj)
-        if ((orf.name != "GAP")): drawObject.regular_polygon(bounding_circle=new, n_sides=3, rotation=(180 - d_end + adj), fill=orf.color, outline="Black")
-        drawObject.arc(circle_xy_a[circ_level], d_start, d_end - adj, orf.color, width=(ORF_width * 2))
+        if (orf.isCompl != 1):
+            new  = _getArrowStart(circle_xy_a[circ_level], d_start, adj, True)
+            if ((orf.name != "GAP")): drawObject.regular_polygon(bounding_circle=new, n_sides=3, rotation=(-adj - d_start), fill=orf.color, outline="Black")
+            adj_start = d_start + adj
+            adj_end = d_end
+        else:
+            new  = _getArrowStart(circle_xy_a[circ_level], d_end, adj, False)
+            if ((orf.name != "GAP")): drawObject.regular_polygon(bounding_circle=new, n_sides=3, rotation=(180 - d_end + adj), fill=orf.color, outline="Black")
+            adj_start = d_start
+            adj_end = d_end - adj
+        
+        drawObject.arc(circle_xy_a[circ_level], adj_start, adj_end, orf.color, width=(ORF_width * 2))
         # отрисовка подписей
         if (orf.name != "GAP"):
             txtpoint  = _getEndXY(circle_xy_a[circ_level], (d_start + d_end) / 2, circle_point_size)
@@ -290,6 +305,7 @@ parser.add_argument("-i", "--file_input", nargs="*", type=str, help="Output file
 parser.add_argument("-width", "--width", type=int, default=700, help="Width of the image (700 pixels as default)")
 parser.add_argument("-name", "--name", type=str, default="Virus name", help="If drawn in segment mode this specifies a virus name")
 parser.add_argument("-o", "--output", type=str, default="default.png", help="Output file")
+parser.add_argument("-format", "--file_format", type=str, default="gb", help="File format of the genome maps. GenBank is default")
 parser.add_argument("-revcompl", "--reverse_compl", action="store_true", help="Sequence is a reverse-complement (draw 5` on the right)")
 parser.add_argument("-dpi", "--image_dpi", type=int, default=300, help="DPI of the obtained image, 300 is default")
 parser.add_argument("-batch", "--batch_mode", action="store_true", help="Treat each gb sequence as a separate virus")
@@ -307,6 +323,7 @@ isBatch     = parser.parse_args().batch_mode
 scale       = parser.parse_args().scale_total
 circular    = parser.parse_args().circular_mode
 drawstops   = parser.parse_args().draw_stop_codons
+fileformat  = parser.parse_args().file_format
 
 
 
@@ -341,18 +358,18 @@ circle_point_size   = int( scale * circle_point_size)
 seqCount = 0
 for file in workload:
 
-    for sequence in SeqIO.parse(file, "gb"):
+    for sequence in SeqIO.parse(file, fileformat):
         add5 = 0
         add3 = 0
         seqCount = seqCount + 1
         sLen = len(sequence.seq)
-        sName = _genSeqName(sequence)
+        sName = _genSeqName(sequence, fileformat)
         allORFs  = []
         lastend = 0
         orfLevel    = 0
         for j in range(0, len(sequence.features)):
-            if(sequence.features[j].type == "CDS" or sequence.features[j].type == "gap"):
-
+            featureType = sequence.features[j].type 
+            if(featureType == "CDS" or featureType == "gap" or featureType == "misc_feature"):
                 orfName     = ""
                 orfColor    = "Purple"
                 orfStart    = int(sequence.features[j].location.start)
@@ -364,7 +381,7 @@ for file in workload:
                 else: orfLevel = 0
                 lastend  = orfEnd
 
-                if (sequence.features[j].type == "CDS"):
+                if (featureType == "CDS" or featureType == "misc_feature"):
                     if (not circular and drawstops): _findStopReadThrough(sequence, orfStart, orfEnd, allORFs, add5, orfLevel)
 
                 try:
@@ -385,14 +402,16 @@ for file in workload:
                 if (("RdRp" in orfProd) or ("polymerase" in orfProd) or (("RdRp" in orfLabel))):
                     orfColor = "Green"
 
+                if(featureType == "misc_feature"):
+                    orfColor = "Blue"
+
                 ################################################################
                 #   Treatment of the gaps
                 #
-                if (sequence.features[j].type == "gap"):
+                if (featureType == "gap"):
                     orfName  = "GAP"
                     orfColor = "Gray"
                     if (orfStart <= 1 and orfEnd == 1):
-                        print("WTF")
                         add5 = int(sequence.features[j].qualifiers["estimated_length"][0])
                         sLen += add5
                         orfStart = -add5
@@ -440,8 +459,8 @@ if (seqCount == 1):
 #calculate some important stuff
 thinkness   = padding_ORF / 2 + ORF_width + padding_ORF + padding_cap_y
 #y-size с запасом, чтобы всё точно влезло
-ySize       = int (padding_top + thinkness * (seqCount + 1) * 1.5 * scale) * 3
-if (circular): ySize = int (padding_top * 3 + xSize + xSize * (seqCount + 1))
+ySize       = int (padding_top + thinkness * (seqCount + 1)) * 3
+if (circular): ySize = int (padding_top * 3 + xSize + xSize * (seqCount))
 
 
 # найти масштаб, используя максимальную длину сгмента и xSize
@@ -464,9 +483,9 @@ image = Image.new("RGBA", (xSize,ySize), (255,255,255,255))
 # чтоб рисовать надо создать отдельный объект ImageDraw
 drawObject = ImageDraw.Draw(image)
 # создать шрифт для прорисовки
-number_font    = ImageFont.truetype("OpenSans-Regular.ttf", num_font_size)
-header_font    = ImageFont.truetype("OpenSans-Regular.ttf", head_font_size)
-title_font    = ImageFont.truetype("OpenSans-Regular.ttf", title_font_size)
+number_font    = ImageFont.truetype(font_file, num_font_size)
+header_font    = ImageFont.truetype(font_file, head_font_size)
+title_font    = ImageFont.truetype(font_file, title_font_size)
 
 nextSegY = padding_top
 maxLevel = 0
